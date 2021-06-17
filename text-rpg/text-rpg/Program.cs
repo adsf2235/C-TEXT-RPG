19bf2279a1f704d3aaf1e30b6c5d6048aa87420c
adsf2235 <81160406+adsf2235@users.noreply.github.com>
2021-06-18 00:55:05 +0900

    Initial Commit

---
+++ text-rpg/text-rpg/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Player
+{
+
+}
+
+class Monster
+{
+
+}
+
+enum STARTSELECT
+{
+    SELECTTOWN,
+    SELECTFILED,
+    NONESELECT
+}
+
+namespace text_rpg
+{
+    class Program
+    {
+        static STARTSELECT StartSelect()
+        {
+            Console.Clear();
+            Console.WriteLine("1.마을");
+            Console.WriteLine("2. 사냥터");
+            Console.WriteLine("어디로 가시겠습니까?");
+            ConsoleKeyInfo CKI = Console.ReadKey();
+            Console.WriteLine("");
+
+            switch (CKI.Key)
+            {
+                case ConsoleKey.D1:
+                    Console.WriteLine("마을로 이동합니다");
+                    Console.ReadKey();
+                    return STARTSELECT.SELECTTOWN;
+                case ConsoleKey.D2:
+                    Console.WriteLine("사냥터로 이동합니다");
+                    Console.ReadKey();
+                    return STARTSELECT.SELECTFILED;
+                default:
+                    return STARTSELECT.NONESELECT;
+
+            }
+
+            
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                StartSelect();
+            }
+            
+        }
+    }
+}
