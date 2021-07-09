61a303ee4d62ce8b2c8c6f065d42774b66808cd1
adsf2235 <81160406+adsf2235@users.noreply.github.com>
2021-07-09 15:41:58 +0900

    Inventory 3rd Start Commit

---
+++ Inventory 3rd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inventory_3rd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
