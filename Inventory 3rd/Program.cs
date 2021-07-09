baa2bb07bc575af04d54eef002f7361aff888a4f
adsf2235 <81160406+adsf2235@users.noreply.github.com>
2021-07-09 15:58:20 +0900

    Create Inven Render

--- Inventory 3rd/Program.cs
+++ Inventory 3rd/Program.cs
@@ -2,11 +2,68 @@
 
 namespace Inventory_3rd
 {
+    class Item
+    {
+        int gold;
+        string name;
+    }
+
+    class Inven
+    {
+        Item[] ArrItem;
+        int XIndex;
+
+        public Inven(int _X, int _Y)
+        {
+            if(_X < 1)
+            {
+                _X = 1;
+            }
+
+            if (_Y <1)
+            {
+                _Y = 1;
+            }
+            ArrItem = new Item[_X * _Y];
+            XIndex = _X;
+
+        }
+
+        public void Render()
+        {
+            for (int i = 0; i < ArrItem.Length; i++)
+            {
+                if (i !=0 && i % XIndex == 0 )
+                {
+                    Console.WriteLine("");
+
+                }
+
+                if (ArrItem[i] != null)
+                {
+                    Console.Write("■");
+                }
+                else
+                {
+                    Console.Write("□");
+                }
+            }
+            
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Inven PInven = new Inven(5, 3);
+
+            while (true)
+            {
+                Console.Clear();
+                PInven.Render();
+                Console.ReadKey();
+            }
         }
     }
 }
