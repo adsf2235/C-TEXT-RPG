039bf4f44280978a637be90424d579e8d1e76cfd
adsf2235 <81160406+adsf2235@users.noreply.github.com>
2021-07-13 19:19:21 +0900

    Tetris Start Commit

---
+++ Testris/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
