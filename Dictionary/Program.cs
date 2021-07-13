79e9772e98fc301d21a7ad652e0ce41f36ec6cd4
adsf2235 <81160406+adsf2235@users.noreply.github.com>
2021-07-13 19:18:43 +0900

    Create Dic Func

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dictionary
 {
@@ -6,7 +7,20 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> NewDic = new Dictionary<string, int>();
+
+            NewDic.Add("일입니다", 1);
+            NewDic.Add("이입니다", 2);
+            NewDic.Add("멀까요", 8941);
+
+            string Key = "멀까요";
+
+            if (true == NewDic.ContainsKey(Key))
+            {
+                Console.WriteLine(NewDic[Key]);
+            }
+            
+
         }
     }
 }
