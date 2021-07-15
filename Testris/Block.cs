2cad82e68d5481ba5cf35e899036b9a629896360
adsf2235 <81160406+adsf2235@users.noreply.github.com>
2021-07-15 15:44:59 +0900

    Create Screen Clear

---
+++ Testris/Block.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testris
+{
+
+    enum BLOCKTYPE
+    {
+        BT_I,
+        BT_L,
+        BT_J,
+        BT_Z,
+        BT_S,
+        BT_T,
+        BT_O,
+    }
+    class Block
+    {
+        TETRISSCREEN TScreen = null;
+
+        int X = 0;
+        int Y = 0;
+
+
+            
+        public Block(TETRISSCREEN _TScreen)
+        {
+            TScreen = _TScreen;
+        }
+
+        public void Input()
+        {
+            if (true == Console.KeyAvailable)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.A:
+                        X -= 1;
+                        break;
+                    case ConsoleKey.D:
+                        X += 1;
+                        break;
+                    case ConsoleKey.S:
+                        Y += 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void Move()
+        {
+            Input();
+            TScreen.SetBlock(Y, X, TBLOCK.WALL);
+        }
+
+    }
+}
---
+++ Testris/TScreen.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testris
+{
+    enum TBLOCK
+    {
+        VOID,
+        WALL,
+
+
+    }
+
+    class TETRISSCREEN
+    {
+        List<List<TBLOCK>> BlockList = new List<List<TBLOCK>>();
+
+        public void SetBlock(int _y, int _x, TBLOCK _TBLOCK)
+        {
+            BlockList[_y][_x] = _TBLOCK;
+        }
+
+        public void Clear()
+        {
+            for (int y = 0; y < BlockList.Count; ++y)
+            {
+                for (int x = 0; x < BlockList[y].Count; ++x)
+                {
+                    BlockList[y][x] = TBLOCK.VOID;
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public void Render()
+        {
+            for (int y = 0; y < BlockList.Count; ++y)
+            {
+                for (int x = 0; x < BlockList[y].Count; ++x)
+                {
+                    switch (BlockList[y][x])
+                    {
+                        case TBLOCK.VOID:
+                            Console.Write("□");
+                            break;
+                        case TBLOCK.WALL:
+                            Console.Write("■");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public TETRISSCREEN(int _X, int _Y)
+        {
+            for (int y = 0; y < _Y; ++y)
+            {
+                BlockList.Add(new List<TBLOCK>());
+                for (int x = 0; x < _X; ++x)
+                {
+                    BlockList[y].Add(TBLOCK.VOID);
+                }
+
+
+            }
+
+            for (int i = 0; i < BlockList[0].Count; i++)
+            {
+                BlockList[0][i] = TBLOCK.WALL;
+            }
+            for (int i = 0; i < BlockList[0].Count; i++)
+            {
+                BlockList[BlockList.Count - 1][i] = TBLOCK.WALL;
+            }
+
+        }
+
+    }
+}
